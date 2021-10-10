46051ebb2549502e674cb395936b33711edefbf2
Matthew Slipper <me@matthewslipper.com>
2021-10-09 22:48:13 -0600

    --wip-- [skip ci]

--- ops/scripts/build-ci.sh
+++ ops/scripts/build-ci.sh
@@ -1,7 +1,7 @@
 # build in 2 steps
 function build_images() {
-    docker-compose build --parallel -- builder l2geth l1_chain
-    docker-compose build --parallel -- deployer dtl batch_submitter relayer integration_tests
+    docker buildx bake -f docker-compose.yml builder l2geth l1_chain --set cache-from=/tmp/.buildx-cache,cache-t0=/tmp/.buildx-cache-new
+    docker buildx bake -f docker-compose.yml deployer dtl batch_submitter relayer integration_tests --set cache-from=/tmp/.buildx-cache,cache-t0=/tmp/.buildx-cache-new
 }
 
 function build_dependencies() {
