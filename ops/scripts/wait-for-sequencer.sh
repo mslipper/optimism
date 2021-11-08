c22090f39087fd9cbce6db29c113b533e048159c
Matthew Slipper <me@matthewslipper.com>
2021-11-08 00:28:42 -0700

    wip

--- ops/scripts/wait-for-sequencer.sh
+++ ops/scripts/wait-for-sequencer.sh
@@ -8,7 +8,9 @@ do
     sleep 3
     if [ $i -eq $RETRIES ]; then
         echo 'Timed out waiting for sequencer'
-        break
+        echo 'Sequencer logs:'
+        docker logs ops_l2geth_1 -n 500
+        exit 1
     fi
     echo 'Waiting for sequencer...'
     ((i=i+1))
