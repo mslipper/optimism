a48cd4429b977321ec9443d0a9618d4c0c4c5f6a
Matthew Slipper <me@matthewslipper.com>
2022-01-27 13:29:06 -0700

    --wip-- [skip ci]

--- integration-tests/test/hardfork.spec.ts
+++ integration-tests/test/hardfork.spec.ts
@@ -192,6 +192,24 @@ describe.only('Hard forks', () => {
             'debug_traceTransaction',
             [tx.hash, { overrides: { berlinBlock: tip.number + 10 } }]
           )
+
+          console.log(JSON.stringify({
+            berlinGas: berlinTrace.gas,
+            preBerlinGas: preBerlinTrace.gas,
+            berlin: berlinTrace.structLogs.map(l => ({
+                pc: l.pc,
+                op: l.op,
+                gas: l.gas,
+                gasCost: l.gasCost,
+              })),
+            preBerlin: preBerlinTrace.structLogs.map(l => ({
+              pc: l.pc,
+              op: l.op,
+              gas: l.gas,
+              gasCost: l.gasCost,
+            })),
+          }, null, '  '))
+
           // Updating to a zero value from a non zero value should becomes
           // more expensive due to this change being coupled with EIP-2929
           expect(berlinTrace.gas).to.be.gt(preBerlinTrace.gas)
