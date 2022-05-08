94a42e26657ca663c4931e66d24f4edc149a034f
Matthew Slipper <me@matthewslipper.com>
2022-05-08 15:53:16 -0600

    --wip-- [skip ci]

---
+++ bedrock-migration/migrate-ovm-eth/cmd/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/mattn/go-isatty"
+	"os"
+	"path/filepath"
+)
+
+var (
+	OVMETHAddress = common.HexToAddress("0x4200000000000000000000000000000000000006")
+)
+
+func main() {
+	log.Root().SetHandler(log.StreamHandler(os.Stderr, log.TerminalFormat(isatty.IsTerminal(os.Stderr.Fd()))))
+
+	args := os.Args
+
+	if len(args) != 3 {
+		log.Crit("must pass in an input and output file")
+	}
+
+	db, err := rawdb.NewLevelDBDatabaseWithFreezer(
+		args[0],
+		0,
+		0,
+		filepath.Join(args[0], "freezer"),
+		"",
+		true,
+	)
+	if err != nil {
+		log.Crit("error opening DB", "err", err)
+	}
+
+	stateDB := state.NewDatabase(db)
+	st, err := stateDB.OpenStorageTrie(OVMETHAddress.Hash(), common.Hash{})
+	if err != nil {
+		log.Crit("error opening storage trie", "err", err)
+	}
+
+	iter := st.NodeIterator(nil)
+
+	for iter.Next(true) {
+		if !iter.Leaf() {
+			continue
+		}
+
+		st.GetKey()
+
+		fmt.Println(hex.EncodeToString(iter.LeafKey()))
+	}
+}
